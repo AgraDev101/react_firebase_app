3d495ac1fed1869605d4df3511984d49c431922e
AgraDev101 <pym.part@gmail.com>
2025-02-16 22:17:41 +0530

    api keys

--- firebase.js
+++ firebase.js
@@ -3,12 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXUvpeDnkWOGsrw_c8lSuNGQor8cQhfSs",
-  authDomain: "full-crud-70b1f.firebaseapp.com",
-  projectId: "full-crud-70b1f",
-  storageBucket: "full-crud-70b1f.firebasestorage.app",
-  messagingSenderId: "586554086367",
-  appId: "1:586554086367:web:f8b2416e6654331a22ea6a"
+
 };
 
 const app = initializeApp(firebaseConfig);
